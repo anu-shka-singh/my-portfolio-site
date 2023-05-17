7dc546fd57d5aaf94f2eca9da51c7936ac82fc0d
anu-shka-singh <99676304+anu-shka-singh@users.noreply.github.com>
2023-05-17 16:51:10 +0000

    site updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anushka Singh",
+  title: "AI & ML enthusiast",
+  email: "anushkasingh2811@gmail.com",
+  gitHub: "https://github.com/anu-shka-singh",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/anushka-singh-630042229/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
